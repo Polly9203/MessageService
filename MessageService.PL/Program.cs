d5a05255e0e1ae0492c59237253bb665d162694c
Polly <apollinaria.9203@yandex.by>
2023-07-26 15:56:53 +0300

    Bug fixing

--- MessageService.PL/Program.cs
+++ MessageService.PL/Program.cs
@@ -10,7 +10,7 @@ Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Debug()
     .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
     .Enrich.FromLogContext()
-    .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
+    .WriteTo.File("C:\\Logs\\log.txt", rollingInterval: RollingInterval.Day)
     .CreateLogger();
 
 builder.Services.AddControllers();
